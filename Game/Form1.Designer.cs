f986f00984a3e861333d99b4b6f20745989aeef7
GholemHub <60587219+GholemHub@users.noreply.github.com>
2020-03-24 14:11:49 +0100

    Добавьте файлы проекта.

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-5, -7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 365);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Game.Res.backGround;
+            this.ClientSize = new System.Drawing.Size(2846, 1325);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.WindowFrame;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+
+        public Bitmap playerTexture = Res.player;
+        public Bitmap animacja = Res.PlayerAnimatoin;
+        public Bitmap GameBackGround = Res.backGround;
+
+        Graphics g;
+        
+        public int SWidth;
+        public int SHeight;
+
+        private Point p;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            SWidth = this.Size.Width;
+            SHeight = this.Size.Height;
+
+            p = new Point(SWidth / 2, SHeight / 2);
+
+            this.KeyDown += new KeyEventHandler(Control);
+
+            DrawMap();
+        }
+        
+
+
+        private void Control(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    p.X -= 100;
+                    break;
+                case Keys.Right:
+                    p.X += 100;
+                    break;
+                case Keys.Down:
+                    p.Y += 100;
+                    break;
+                case Keys.Up:
+                    p.Y -= 100;
+                    break;
+            }
+            DrawMap();
+        }
+
+        /*  TheMap[] BigMap =
+          {
+              new TheMap(),
+              new TheMap(),
+              new TheMap()
+          };
+          */
+
+        public static object Resource1 { get; private set; }
+
+        private void DrawMap()
+        {
+           pictureBox1.Size = new Size(this.Size.Width, this.Size.Height);
+           pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            
+            Image Part = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(Part);
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    int fielSizeX = pictureBox1.Width / 10;
+                    int fielSizeY = pictureBox1.Height / 10;
+
+                    Color c = Color.Red;                    
+                    pictureBox1.Image = Part;
+                    g.FillEllipse(new SolidBrush(c),
+                                      i * fielSizeX,
+                                      j * fielSizeY,
+                                      fielSizeX,
+                                      fielSizeY);
+                }
+            }
+            g.DrawImage(playerTexture, new Rectangle(p.X, p.Y, 100, 100));
+        }
+    }
+
+    public class TheMap
+    {
+        public Bitmap Fon = Res.player;
+        TheMap(Bitmap F){ Fon = F; }
+        TheMap(){ Fon = Res.player; }
+    }
+}
